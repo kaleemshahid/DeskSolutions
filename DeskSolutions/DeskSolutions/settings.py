9008360390996d660134d86549fcf8f4b1e01b63
Hafiz Waqas <waqas@altairlabs.com>
2020-12-10 22:10:46 +0500

    use TokenAuthentication

--- DeskSolutions/DeskSolutions/settings.py
+++ DeskSolutions/DeskSolutions/settings.py
@@ -52,11 +52,11 @@ GROUP_ALLOCATE = "OrganizationAdministrators"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
