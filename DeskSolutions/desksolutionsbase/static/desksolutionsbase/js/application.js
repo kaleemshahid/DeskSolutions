44f21cd4ef3f1385cecbec13e392fda7ae73815a
Kaleem Shahid <kaleemshahid20@gmail.com>
2020-12-20 02:36:39 +0500

    making contact form

--- DeskSolutions/desksolutionsbase/static/desksolutionsbase/js/application.js
+++ DeskSolutions/desksolutionsbase/static/desksolutionsbase/js/application.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
 
-    // $('#modal_open_btn').on('click', function () {
-    //     // $('#app_form_id').trigger('reset');
-    //     $('#app_form_id')[0].reset();
-    // })
+    $('.modal_open_btn').on('click', function () {
+        console.log("modal open btn")
+        $('.application-upload').trigger('reset');
+        // $('#app_form_id')[0].reset();
+        $(".application_form_error").hide()
+    })
     // $('#btnClose').on('click', function () {
     //     alert("btn close")
     //     $('#app_form_id').trigger('reset');
     // })
-    $('#modal').on('hidden.bs.modal', function(){
-        $('#app_form_id').trigger('reset');
-    })
+    // $('#modal').on('hidden.bs.modal', function(){
+    //     console.log("application upload")
+    //     $('.application-upload').trigger('reset');
+    // })
 
-    $("#success-application").hide()
+    $(".success-application").hide()
     $('.application-upload').submit(function (e) {
         e.preventDefault()
         var formdata = new FormData(this)
@@ -24,6 +27,7 @@ $(document).ready(function(){
             data: formdata,
             contentType: false,
             processData: false,
+
             success: (response) => {
                 // var form_msg = response
                 var form_msg = typeof(response)
@@ -46,8 +50,16 @@ $(document).ready(function(){
                         var id = '#form-' + i + '-error'
                         console.log(i)
                         console.log(response[i])
+                        console.log(id)
+                        $(".application_form_error").show()
                         $(id).text(response[i])
+                        // $(document).ajaxStop(function() {
+                        //     alert("adjasdnk")
                         
+                        setTimeout(function() {
+                            $(".application_form_error").fadeOut('fast');
+                        }, 7000)
+                        // });
                         // var id = '#id_' + i
                         // $(id).parent().append(form_msg[i])
                         
@@ -56,20 +68,24 @@ $(document).ready(function(){
                         // var id = '#id_' + i
                         // $(id).parent('p').append(response[i])
                     }
+ 
                 } else {
                     console.log("response ni mila")
                     // $('.application-upload').parent().html("<p class=''>Application submitted successfully! The company will contact you upon qualification")
                     // $("#submit-application-btn").hide()
-                    $(".modal").modal('toggle')
-                    $('#app_form_id').trigger('reset');
+                    $(".modal").modal('hide')
+                    // $('#app_form_id').trigger('reset')
+                    $('.modal').trigger('reset')
                     $("#success-application").show()
                     $(".application_form_error").hide()
                     setTimeout(function() {
                         $('#success-application').fadeOut('fast');
-                    }, 3000)
+                    }, 7000)
                     
                 }
 
+                
+                
             },
             error: function (response) {
                 console.log(response)
@@ -80,10 +96,11 @@ $(document).ready(function(){
         })
     })
 
-    $('[data-role=upButton]').click(function(){
-        var post_id = $(this).attr('job_id');
-        var app_id = $("[data-role=application]").attr('app_id');
-        // post_id.modal()
-      });
+    // $('[data-role=upButton]').click(function(){
+    //     var post_id = $(this).attr('job_id');
+    //     var app_id = $("[data-role=application]").attr('app_id');
+    //     // post_id.modal()
+    //   });
+    
 
 })
