3e6d29e39b592bef374c806236e5af05ebe142f4
Hafiz Waqas <waqas@altairlabs.com>
2020-12-22 18:45:39 +0500

    add some additional fields in login response

--- DeskSolutions/account/views.py
+++ DeskSolutions/account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import User
+from .models import User, Profile
 from .serializers import UserSerializer
 from rest_framework import viewsets, permissions, status
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -27,10 +27,25 @@ class UserLoginApiView(ObtainAuthToken):
         if not created:
             token.delete()
             token = Token.objects.create(user=user)
+
+        # getting role of the login user
+        profiles = Profile.objects.filter(organization=token.user_id)
+        role = "admin"
+        if not profiles:
+            pass
+        elif profiles.first().is_manager:
+            role = "manager"
+        elif not profiles.first().is_manager:
+            role = "employee"
+
         return Response(
             {
                 'token': token.key,
-                'created_at': token.created,
+                'role': role,
+                'first_name':token.user.first_name,
+                'last_name': token.user.last_name,
+                'created_at': token.user.date_joined,
+
             }
         )
 
@@ -42,3 +57,8 @@ class LogoutApiView(APIView):
         print(request.user.email)
         request.user.auth_token.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# m1 vvqsXO6
+# e1 b0Lapi8   iX6fZcm
+# e2 aNLyzev
