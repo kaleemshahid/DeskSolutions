3957742f49e7307f9ba7500bb0b2f83410cb432f
Kaleem Shahid <kaleemshahid20@gmail.com>
2021-01-11 05:24:59 +0500

    media dir setting 7

--- DeskSolutions/settings.py
+++ DeskSolutions/settings.py
@@ -274,9 +274,9 @@ STATIC_URL = '/static/'
 #location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # location where you will store your static files
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'media')
+]
 
 # Handle media files
 
