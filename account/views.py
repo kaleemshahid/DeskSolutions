df9e9e75b4df23f6b8bb710549b589747e12a44c
Kaleem Shahid <kaleemshahid20@gmail.com>
2021-03-15 19:22:48 +0500

    complaint box issue 3

--- account/views.py
+++ account/views.py
@@ -201,7 +201,24 @@ class ComplaintBoxViewSet(ListAPIView, CreateAPIView):
     queryset = ComplaintBox.objects.all()
     serializer_class = CreateComplaintBoxSerializer
 
-    def create(self, request, *args, **kwargs):
+    # def create(self, request, *args, **kwargs):
+    #     if self.request.user.is_admin:
+    #         raise NotAcceptable("Admin can not create issue")
+    #     request_data = request.data
+    #     print(request_data)
+    #     request_data.update({
+    #         "user_profile": request.user.id
+    #     })
+    #     # self.request.data._mutable = True
+    #     serializer = self.get_serializer(data=request.data)
+    #     print(serializer)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_create(serializer)
+    #     headers = self.get_success_headers(serializer.data)
+    #     print(serializer.data.copy())
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def post(self, request, *args, **kwargs):
         if self.request.user.is_admin:
             raise NotAcceptable("Admin can not create issue")
         request_data = request.data
@@ -213,7 +230,8 @@ class ComplaintBoxViewSet(ListAPIView, CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         print(serializer)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        # self.perform_create(serializer)
+        serializer.save()
         headers = self.get_success_headers(serializer.data)
         print(serializer.data.copy())
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
